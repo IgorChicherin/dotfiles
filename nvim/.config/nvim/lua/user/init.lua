f5abc175a0cee8a413fbfcc264d3263067ac34ba
ashzak <ashzak990@gmail.com>
2024-03-06 10:21:37 +0300

    gitignore plugin

--- nvim/.config/nvim/lua/user/init.lua
+++ nvim/.config/nvim/lua/user/init.lua
@@ -1,6 +1,12 @@
 return {
-    colorscheme = "astrodark",
-    polish = function()
-        require("cmake-tools").setup {}
-      end,
+        colorscheme = "astrodark",
+        mappings = {
+            n = {
+            }
+        },
+        polish = function()
+                require("cmake-tools").setup {}
+		local gitignore = require("gitignore")
+                vim.keymap.set("n", "<leader>gi", gitignore.generate, {desc = "Gitignore generate"})
+            end,
 }
