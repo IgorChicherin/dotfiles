7d41b4fe1d53c4c0f0766a4624b446acd8b74dbf
Igor <ashzak990@gmail.com>
2024-03-09 13:09:00 +0300

    gnome custom themes

---
+++ gnome-themes/.themes/dark-menu.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+dconf write /org/gnome/shell/extensions/arcmenu/menu-background-color "'rgba(48,48,49, 0.88)'"  
+dconf write /org/gnome/shell/extensions/arcmenu/menu-foreground-color "'rgba(223, 223, 223, 1)'"
+dconf write /org/gnome/shell/extensions/arcmenu/menu-item-active-bg-color "'rgb(25, 98, 163)'"
+dconf write /org/gnome/shell/extensions/arcmenu/menu-item-active-fg-color "'rgb(255, 255, 255)'"
+dconf write /org/gnome/shell/extensions/arcmenu/menu-item-hover-bg-color "'rgb(21, 83, 158)'"
+dconf write /org/gnome/shell/extensions/arcmenu/menu-item-hover-fg-color "'rgb(255, 255, 255)'"
+dconf write /org/gnome/shell/extensions/user-theme/name "'Orchis-Dark'"
+dconf write /org/gnome/shell/extensions/arcmenu/reload-theme true
---
+++ gnome-themes/.themes/light-menu.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+dconf write /org/gnome/shell/extensions/arcmenu/menu-background-color "'rgba(245,247,250, 0.95)'"  
+dconf write /org/gnome/shell/extensions/arcmenu/menu-foreground-color "'rgba(18, 51, 84, 1)'"
+dconf write /org/gnome/shell/extensions/arcmenu/menu-item-active-bg-color "'rgba(18,51,84,0.15)'"
+dconf write /org/gnome/shell/extensions/arcmenu/menu-item-active-fg-color "'rgb(18,51,84)'"
+dconf write /org/gnome/shell/extensions/arcmenu/menu-item-hover-bg-color "'rgb(21, 83, 158)'"
+dconf write /org/gnome/shell/extensions/arcmenu/menu-item-hover-fg-color "'rgb(18,51,84)'"
+dconf write /org/gnome/shell/extensions/user-theme/name "'Orchis'"
+dconf write /org/gnome/shell/extensions/arcmenu/reload-theme true
