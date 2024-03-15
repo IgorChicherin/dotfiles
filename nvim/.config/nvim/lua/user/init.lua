8b7a6cd9e3533cfb4252c708672459f666cf7af1
Ingwar <ashzak990@gmail.com>
2024-03-15 23:17:50 +0300

    hop added

--- nvim/.config/nvim/lua/user/init.lua
+++ nvim/.config/nvim/lua/user/init.lua
@@ -1,9 +1,19 @@
 return {
     colorscheme = "astrodark",
-    mappings = {n = {["<A-j>"] = {":m+<CR>"}, ["<A-k>"] = {":m-2<CR>"}}},
+    mappings = {
+        n = {
+            ["<A-j>"] = {":m+<CR>"},
+            ["<A-k>"] = {":m-2<CR>"},
+            ["<leader>hw"] = {":HopWord<CR>", desc = "Hop Word"},
+            ["<leader>hc"] = {":HopChar1<CR>", desc = "Hop one character"},
+            ["<leader>ht"] = {":HopChar2<CR>", desc = "Hop two characters"},
+            ["<leader>hp"] = {":HopPattern<CR>", desc = "Hop two pattern"}
+        }
+    },
     polish = function()
         require "user.configs.cmake-tools"
         require "user.configs.gitignore"
         require "user.configs.codeium"
+        require "user.configs.hop"
     end
 }
