bf197f3c9f8897429c467ccf160f18ec8f2051fe
Ingwar <ashzak990@gmail.com>
2024-03-15 22:00:43 +0300

    surround brackets plugin

--- nvim/.config/nvim/lua/user/init.lua
+++ nvim/.config/nvim/lua/user/init.lua
@@ -1,11 +1,6 @@
 return {
     colorscheme = "astrodark",
-    mappings = {
-        n = {
-            ["<A-j>"] = {":m+<CR>"},
-            ["<A-k>"] = {":m-2<CR>"}
-        }
-    },
+    mappings = {n = {["<A-j>"] = {":m+<CR>"}, ["<A-k>"] = {":m-2<CR>"}}},
     polish = function()
         require "user.configs.cmake-tools"
         require "user.configs.gitignore"
