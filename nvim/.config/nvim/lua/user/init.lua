8faf6d8c9eda2219e1e85a8a968f5fe020405080
Igor Chicherin <ashzak990@gmail.com>
2024-03-20 14:57:58 +0300

    remap home page

--- nvim/.config/nvim/lua/user/init.lua
+++ nvim/.config/nvim/lua/user/init.lua
@@ -2,32 +2,31 @@ return {
     colorscheme = "astrodark",
     mappings = {
         n = {
-            ["<A-j>"] = {":m+<CR>"},
-            ["<A-k>"] = {":m-2<CR>"},
-            ["<leader>hw"] = {":HopWord<CR>", desc = "Hop Word"},
-            ["<leader>hc"] = {":HopChar1<CR>", desc = "Hop one character"},
-            ["<leader>ht"] = {":HopChar2<CR>", desc = "Hop two characters"},
-            ["<leader>hp"] = {":HopPattern<CR>", desc = "Hop two pattern"},
+            ["<A-j>"] = { ":m+<CR>" },
+            ["<A-k>"] = { ":m-2<CR>" },
+            ["<leader>h"] = { "", desc = "Hop" },
+            ["<leader>hw"] = { ":HopWord<CR>", desc = "Hop Word" },
+            ["<leader>hc"] = { ":HopChar1<CR>", desc = "Hop one character" },
+            ["<leader>ht"] = { ":HopChar2<CR>", desc = "Hop two characters" },
+            ["<leader>hp"] = { ":HopPattern<CR>", desc = "Hop two pattern" },
             ["<tab>"] = {
                 function()
-                    require("astronvim.utils.buffer").nav(vim.v.count > 0 and
-                                                              vim.v.count or 1)
+                    require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1)
                 end,
-                desc = "Next buffer"
+                desc = "Next buffer",
             },
             ["<S-tab>"] = {
                 function()
-                    require("astronvim.utils.buffer").nav(
-                        -(vim.v.count > 0 and vim.v.count or 1))
+                    require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1))
                 end,
-                desc = "Previous buffer"
-            }
-        }
+                desc = "Previous buffer",
+            },
+        },
     },
     polish = function()
-        require "user.configs.cmake-tools"
-        require "user.configs.gitignore"
-        require "user.configs.codeium"
-        require "user.configs.hop"
-    end
+        require("user.configs.cmake-tools")
+        require("user.configs.gitignore")
+        require("user.configs.codeium")
+        require("user.configs.hop")
+    end,
 }
