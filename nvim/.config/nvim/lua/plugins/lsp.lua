ce971770f6022377d294bd1b603bf5e5d42d26fc
Igor Chicherin <ashzak990@gmail.com>
2025-01-21 21:37:13 +0300

    fix

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,4 +1,7 @@
 return {
+	{ import = "lazyvim.plugins.extras.lang.go" },
+	{ import = "lazyvim.plugins.extras.lang.python" },
+	{ import = "lazyvim.plugins.extras.coding.nvim-cmp" },
 	{
 		-- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
 		-- used for completion, annotations and signatures of Neovim apis
