04cb45562d1cf19b6cfbbbdd8ce4978cd5c5763a
Igor Chicherin <ashzak990@gmail.com>
2025-01-21 23:26:18 +0300

    test

--- nvim/.config/nvim/lua/plugins/lazy.lua
+++ nvim/.config/nvim/lua/plugins/lazy.lua
@@ -1,10 +1,10 @@
-return {
-	{
-		"LazyVim/LazyVim",
-		opts = {
-			defaults = {
-				keymaps = false, -- lazyvim.config.keymaps
-			},
-		},
-	},
-}
+-- return {
+-- 	{
+-- 		"LazyVim/LazyVim",
+-- 		opts = {
+-- 			defaults = {
+-- 				keymaps = false, -- lazyvim.config.keymaps
+-- 			},
+-- 		},
+-- 	},
+-- }
--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,13 +1,12 @@
--- LSP keymaps
-return {
-	{
-		"neovim/nvim-lspconfig",
-		opts = function()
-			local keys = require("lazyvim.plugins.lsp.keymaps").get()
-			-- change a keymap
-			keys[#keys + 1] = { "<CR>", "<cmd>:lua cmp.mapping.confirm({select = true})<cr>" }
-			keys[#keys + 1] = { "<C-n>", false }
-			keys[#keys + 1] = { "<C-p>", false }
-		end,
-	},
-}
+-- return {
+-- 	{
+-- 		"neovim/nvim-lspconfig",
+-- 		opts = function()
+-- 			local keys = require("lazyvim.plugins.lsp.keymaps").get()
+-- 			-- change a keymap
+-- 			keys[#keys + 1] = { "<CR>", "<cmd>:lua cmp.mapping.confirm({select = true})<cr>" }
+-- 			keys[#keys + 1] = { "<C-n>", false }
+-- 			keys[#keys + 1] = { "<C-p>", false }
+-- 		end,
+-- 	},
+-- }
