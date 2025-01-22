f46c4d72ab69b61cb923f1637e0f14e263b3a753
Igor Chicherin <ashzak990@gmail.com>
2025-01-22 08:58:53 +0300

    f

--- nvim/.config/nvim/lua/config/autocmds.lua
+++
@@ -1,7 +0,0 @@
-vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-	group = "insert_jk_delete",
-	callback = function(event)
-		vim.keymap.del("i", "<A-j>")
-		vim.keymap.del("i", "<A-k>")
-	end,
-})
--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,12 +1,15 @@
 -- return {
--- 	{
--- 		"neovim/nvim-lspconfig",
--- 		opts = function()
--- 			local keys = require("lazyvim.plugins.lsp.keymaps").get()
--- 			-- change a keymap
--- 			keys[#keys + 1] = { "<CR>", "<cmd>:lua cmp.mapping.confirm({select = true})<cr>" }
--- 			keys[#keys + 1] = { "<C-n>", false }
--- 			keys[#keys + 1] = { "<C-p>", false }
--- 		end,
--- 	},
+-- {
+-- 	"neovim/nvim-lspconfig",
+-- 	{ import = "lazyvim.plugins.extras.coding.nvim-cmp" },
+-- 	opts = function()
+-- 		local keys = require("lazyvim.plugins.lsp.keymaps").get()
+-- 		local cmp = require("cmp")
+-- 		-- change a keymap
+-- 		keys[#keys + 1] = { "<CR>", "<cmd>:lua cmp.mapping.confirm({select = true})<cr>" }
+-- 		keys[#keys + 1] = { "<C-n>", false }
+-- 		keys[#keys + 1] = { "<C-p>", false }
+-- 		keys[#keys + 1] = { "<A-j>", cmp.mapping.select_next_item }
+-- 	end,
+-- },
 -- }
