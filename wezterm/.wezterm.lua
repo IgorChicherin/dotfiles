83d1a30b438418c9a682b54a316a0dae9a063ae5
Igor Chicherin <ashzak990@gmail.com>
2025-01-23 11:33:26 +0300

    wezterm config added

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,34 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+local function scheme_for_appearance(appearance)
+	if appearance:find("Dark") then
+		return "Tokyo Night"
+	else
+		return "Tokyo Night Day"
+	end
+end
+
+wezterm.on("window-config-reloaded", function(window, pane)
+	local overrides = window:get_config_overrides() or {}
+	local appearance = window:get_appearance()
+	local scheme = scheme_for_appearance(appearance)
+	if overrides.color_scheme ~= scheme then
+		overrides.color_scheme = scheme
+		window:set_config_overrides(overrides)
+	end
+end)
+
+config.enable_tab_bar = false
+
+-- Hyprland fix
+config.enable_wayland = false
+
+-- and finally, return the configuration to wezterm
+return config
