bedb4eff5f2ec0d6865a6cf78981a1595590e28e
Igor Chicherin <igor.chicherin@mtrading.com>
2025-03-02 11:21:26 +0300

    colorscheme config fix

--- nvim/.config/nvim/lua/plugins/autodarkmode.lua
+++ nvim/.config/nvim/lua/plugins/autodarkmode.lua
@@ -1,16 +1,16 @@
 return {
-  "f-person/auto-dark-mode.nvim",
-  opts = {
-    update_interval = 1000,
-    set_dark_mode = function()
-      vim.api.nvim_set_option_value("background", "dark", {})
-      vim.cmd("colorscheme tokyonight-moon")
-    end,
-    set_light_mode = function()
-      vim.api.nvim_set_option_value("background", "light", {})
-      vim.cmd("colorscheme tokyonigh-day")
-    end,
-    -- for gnome dection we need fallback
-    fallback = "light",
-  },
+	"f-person/auto-dark-mode.nvim",
+	opts = {
+		update_interval = 1000,
+		set_dark_mode = function()
+			vim.api.nvim_set_option_value("background", "dark", {})
+			require("tokyonight").load({ style = "moon" })
+		end,
+		set_light_mode = function()
+			vim.api.nvim_set_option_value("background", "light", {})
+			require("tokyonight").load({ style = "day" })
+		end,
+		-- for gnome dection we need fallback
+		fallback = "light",
+	},
 }
