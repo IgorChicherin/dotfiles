6dbf07c585b8c51895e955c0d5c30a686ded68f5
Igor Chicherin <ashzak990@gmail.com>
2025-12-19 22:53:14 +0300

    terminal apps

--- niri/niri-install.sh
+++ niri/niri-install.sh
@@ -1,3 +1,4 @@
 sudo pacman -Suy && sudo pacman -S --needed --noconfirm git base-devel
 git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+cat terminal-apps.lst | tr '\n' ' ' | xargs yay -S --needed --noconfirm --asdeps
 cat apps.lst | tr '\n' ' ' | xargs yay -S --needed --noconfirm --asdeps
